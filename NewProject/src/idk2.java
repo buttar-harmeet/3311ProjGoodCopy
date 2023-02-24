a5716cac248efca9c1883127ba641b33c000129b
buttar-harmeet <harmeetb@my.yorku.ca>
2023-02-24 16:36:15 -0500

    Project File Tree

---
+++ NewProject/src/idk2.java
@@ -0,0 +1,10 @@
+
+public class idk2 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		idk dao = new idk();
+        dao.readDataBase();
+	}
+
+}

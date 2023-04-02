2e48e4acfddfed49d6a1d5738ed10073e00d47f2
Jaideep Kular <kularjaideep@gmail.com>
2023-04-01 21:58:01 -0400

    topbar add new datasets

--- 3311Project/src/Main.java
+++ 3311Project/src/Main.java
@@ -20,10 +20,83 @@ public class Main {
 
     public static void createMainFrame() {
     	JFrame f = new JFrame("NHPI Statistics");
+    	    	
+//        f.getContentPane().add(addDataSet(), BorderLayout.NORTH);
+//        f.getContentPane().add(addDataSet(), BorderLayout.NORTH);
+        
+        JPanel topbar = new JPanel();
+        topbar.setLayout(new BoxLayout(topbar, BoxLayout.Y_AXIS));
+        addDataSet(topbar);
+        addDataSet(topbar);
+        
+        JButton btn = new JButton("Add dataset");
+        btn.addActionListener(e -> {
+        	topbar.remove(topbar.getComponentCount() -  1);
+        	addDataSet(topbar);
+        	topbar.add(btn);
+        	f.validate();
+        	f.repaint();
+        });
+        topbar.add(btn);
+        
+        f.getContentPane().add(topbar, BorderLayout.NORTH);
+        
+        JPanel buttons = new JPanel();
 
-        // Set top bar
+        JButton viewTables = new JButton("View Tables / Raw Data");
+        //viewTables.setBounds(50, 100, 200, 300);
+        viewTables.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                tableFrame();
+            }
+        });
+
+        JButton viewCharts = new JButton("View Graphs / Charts");
+        //viewCharts.setBounds(300, 100, 200, 300);
+        viewCharts.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                createChartFrame();
+            }
+        });
+
+        JButton viewStatistics = new JButton("View Statistical Tests");
+       // viewStatistics.setBounds(550, 100, 200, 300);
+        viewStatistics.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                statisticalTestFrame();
+            }
+        });
+
+        JButton viewMachineTest = new JButton("View Machine Tests");
+        //viewMachineTest.setBounds(800, 100, 200, 300);
+        viewMachineTest.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+            	machineTestFrame();
+            }
+        });
+
+        buttons.add(viewTables);
+        buttons.add(viewCharts);
+        buttons.add(viewStatistics);
+        buttons.add(viewMachineTest);
+        
+        f.getContentPane().add(buttons, BorderLayout.CENTER);
+        
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(1050, 600);
+        //f.setLayout(null);
+        f.setVisible(true);
+    }
+    
+    public static void addDataSet(JPanel container) {
+    	JPanel panel = new JPanel();
+    	// Set top bar
         JLabel chooseCountryLabel = new JLabel("Choose a Province: ");
-        chooseCountryLabel.setBounds(100, 10, 200, 100);
+        //chooseCountryLabel.setBounds(100, 10, 200, 100);
         Vector<String> provincesNames = new Vector<String>();
         provincesNames.add("Ontario");
         provincesNames.add("Quebec");
@@ -36,73 +109,28 @@ public class Main {
         provincesNames.add("Nunavut");
         provincesNames.sort(null);
         JComboBox<String> countriesList = new JComboBox<String>(provincesNames);
-        countriesList.setBounds(250, 10, 100, 100 );
+        //countriesList.setBounds(250, 10, 100, 100 );
 
         JLabel from = new JLabel("From");
-        from.setBounds(400, 10, 100, 100);
+        //from.setBounds(400, 10, 100, 100);
         JLabel to = new JLabel("To");
-        to.setBounds(700, 10, 100, 100);
+        //to.setBounds(700, 10, 100, 100);
         Vector<String> years = new Vector<String>();
         for (int i = 2021; i >= 2010; i--) {
             years.add("" + i);
         }
         JComboBox<String> fromList = new JComboBox<String>(years);
-        fromList.setBounds(450, 10, 200, 100);
+        //fromList.setBounds(450, 10, 200, 100);
         JComboBox<String> toList = new JComboBox<String>(years);
-        toList.setBounds(750, 10, 200, 100);
+        //toList.setBounds(750, 10, 200, 100);
 
-        f.add(chooseCountryLabel);
-        f.add(countriesList);
-        f.add(from);
-        f.add(fromList);
-        f.add(to);
-        f.add(toList);
-
-
-        JButton viewTables = new JButton("View Tables / Raw Data");
-        viewTables.setBounds(50, 100, 200, 300);
-        viewTables.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-                tableFrame();
-            }
-        });
-
-        JButton viewCharts = new JButton("View Graphs / Charts");
-        viewCharts.setBounds(300, 100, 200, 300);
-        viewCharts.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-                createChartFrame();
-            }
-        });
-
-        JButton viewStatistics = new JButton("View Statistical Tests");
-        viewStatistics.setBounds(550, 100, 200, 300);
-        viewStatistics.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-                statisticalTestFrame();
-            }
-        });
-
-        JButton viewMachineTest = new JButton("View Machine Tests");
-        viewMachineTest.setBounds(800, 100, 200, 300);
-        viewMachineTest.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-            	machineTestFrame();
-            }
-        });
-
-        f.add(viewTables);
-        f.add(viewCharts);
-        f.add(viewStatistics);
-        f.add(viewMachineTest);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(1050, 600);
-        f.setLayout(null);
-        f.setVisible(true);
+        panel.add(chooseCountryLabel);
+        panel.add(countriesList);
+        panel.add(from);
+        panel.add(fromList);
+        panel.add(to);
+        panel.add(toList);
+        container.add(panel);
     }
 
 
